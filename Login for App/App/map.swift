9c1774e6d95ccce48aaffec5b0bba55e064fba69
Samarium99 <sarah.mo@csedge.org>
2024-05-31 17:37:17 -0400

    test commit
    testing

---
+++ Login for App/App/map.swift
@@ -0,0 +1,17 @@
+//
+//  map.swift
+//  Login for App
+//
+//  Created by Student on 5/16/24.
+//
+
+import Foundation
+import MapKit
+import SwiftUI
+
+//Map(interactionModes: [.zoom])
+  //  .mapStyle(.hybrid)
+
+//preview {
+   // mapview
+//}
